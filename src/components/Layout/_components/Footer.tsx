09d45085946b3d2093a034b90d7e492e5c38a1d2
Saki279 <saki-lovetdr.1029@docomo.ne.jp>
2025-08-03 13:45:48 +0900

    チュートリアル追加

--- src/components/Layout/_components/Footer.tsx
+++ src/components/Layout/_components/Footer.tsx
@@ -1,3 +1,5 @@
+import { Link } from 'react-router-dom';
+
 export default function Footer() {
   const currentYear = new Date().getFullYear();
 
@@ -5,19 +7,32 @@ export default function Footer() {
     <footer className="footer">
       <div className="footer-container">
         <div className="footer-content">
-          <div className="footer-section">
+          {/* <div className="footer-section">
             <h3 className="footer-title">フッターエリア</h3>
             <p className="footer-description">
               フッターコンテンツ
             </p>
-          </div>
+          </div> */}
           <div className="footer-section">
-            <h4 className="footer-subtitle">リンク</h4>
-            <p>リンク一覧</p>
+            {/* <h4 className="footer-subtitle">リンク</h4> */}
+            <ul className="nav-list">
+            <li className="nav-item">
+              <Link to="/" className="nav-link">ホーム</Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/create" className="nav-link">作成</Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/ai-create" className="nav-link">AI作成</Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/panel" className="nav-link">パネル</Link>
+            </li>
+          </ul>
           </div>
         </div>
         <div className="footer-bottom">
-          <p>&copy; {currentYear} Starry Night. All rights reserved.</p>
+          <p>&copy; {currentYear} Uo/Code. All rights reserved.</p>
         </div>
       </div>
     </footer>
